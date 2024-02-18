12537e69b86158df085736b6a54ab2b8e5cf4f95
Mohammed Zaid <mohammedzee98@gmail.com>
2024-02-18 13:02:44 +0530

    elements edited

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -5,15 +5,17 @@ import App from './App.jsx'
 const anotherElement = (
   <a href="https://google.com" target='_blank'>Visit Google</a>
 )
+const anotherUser = "Chai aur React"
 
 const reactElement = React.createElement(
   'a',
   {href: 'https://google.com', target: '_blank'},
-  'click me to visit google'
+  'click me to visit google',
+   anotherUser
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    reactElement
+   reactElement
   
 )
