1f73edb2f9092ddc88d4531f224f2cd569bc9d3c
Mohammed Zaid <mohammedzee98@gmail.com>
2024-02-18 12:45:22 +0530

    new elements added

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 const anotherElement = (
-  <a href='https://google.com' target='_blank'>Visit Google</a>
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click me to visit google'
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    anotherElement
+    reactElement
   
 )
