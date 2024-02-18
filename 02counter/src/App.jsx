cb5deb16b50ab5a3504347e7a904720d0afef553
Mohammed Zaid <mohammedzee98@gmail.com>
2024-02-18 16:12:47 +0530

    created hook useState for value updation

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,13 +5,35 @@ import './App.css'
 
 function App() {
 
+  let [counter, setCounter] = useState(15)
+
+ // let counter = 15
+  const addValue = () => {
+    console.log("clicked", counter);
+    //counter += 1
+    if (counter < 30) {
+      setCounter(counter + 1)
+    }
+  }
+
+  const removeValue = () => {
+    if (counter > 0){
+      setCounter(counter - 1)
+    }
+  }
+
   return (
     <>
     <h1>Chai aur Zaid</h1>
-    <h2>Counter value: 5</h2>
-    <button>Add Value</button>
+    <h2>Counter value: {counter}</h2>
+    <button
+     onClick={addValue}
+     >Add Value {counter}</button>
     <br/>
-    <button>Remove Value</button>
+    <button
+     onClick={removeValue}
+     >Remove Value {counter}</button>
+    <p>Footer: {counter}</p>
     </>
   )
 }
