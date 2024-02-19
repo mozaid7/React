35899adba5b256a6d650e19a212487a82c90af9e
Mohammed Zaid <mohammedzee98@gmail.com>
2024-02-19 13:27:28 +0530

    making changes in Tailwind

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -8,9 +8,29 @@ function App() {
 
   return (
     <>
-    <h1 className="bg-green-400 text-3xl font-bold underline">
-      Hello world!
+    <h1 className="bg-green-400 text-black p-4 rounded-xl">
+      Tailwind Test!
     </h1>
+    <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+  <img class="w-24 h-24 rounded-full mx-auto" src="src/pp1.jpg" alt="" width="384" height="512"/>
+  <div class="pt-6 space-y-4">
+    <blockquote>
+      <p class="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption cl>
+      <div>
+        Sarah Dayan
+      </div>
+      <div>
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+</figure>
     </>
   )
 }
