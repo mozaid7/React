563390abcb45ab02ab0bef1bd7afc64e8a727d2a
Mohammed Zaid <mohammedzee98@gmail.com>
2024-02-19 13:35:19 +0530

    imported card to app.jsx

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './Components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
       Tailwind Test!
     </h1>
+    <Card />
     
     </>
   )
