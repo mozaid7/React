81a15f9df3303250508ed8955d7b1c7324be45eb
Mohammed Zaid <mohammedzee98@gmail.com>
2024-02-19 13:48:32 +0530

    adding elements

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -6,12 +6,16 @@ import Card from './Components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj = {
+    username: "zaid",
+    age: 21
+  }
+  let newArr = [1, 2, 3]
 
   return (
     <>
-    <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
-      Tailwind Test!
-    </h1>
+    <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">Tailwind Test!</h1>
+    <Card channel="ChaiaaurZaid" someObj={newArr} />
     <Card />
     
     </>
