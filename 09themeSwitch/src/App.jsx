3b32038973d141f76e12d11bb5747435ccad9077
Mohammed Zaid <mohammedzee98@gmail.com>
2024-02-21 20:48:21 +0530

    useEffect was called

--- 09themeSwitch/src/App.jsx
+++ 09themeSwitch/src/App.jsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import Card from './Components/Card'
+import ThemeBtn from './Components/ThemeBtn'
 import { ThemeProvider } from './Contexts/Theme'
 
 function App() {
@@ -24,12 +27,12 @@ function App() {
     <div className="flex flex-wrap min-h-screen items-center">
     <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          {/*themeBtn */}
+          <ThemeBtn/>
             
         </div>
 
         <div className="w-full max-w-sm mx-auto">
-           {/* Card*/}
+           <Card/>
         </div>
     </div>
     </div>
