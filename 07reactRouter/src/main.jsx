2b97e08f76a5c564c6648592fcf8e6db3894c750
Mohammed Zaid <mohammedzee98@gmail.com>
2024-02-21 12:42:31 +0530

    adding path through router browser

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx'
 
 
 const router = createBrowserRouter ([
-  
+  {
+    path: '/',
+    element: <Layout />
+  }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
