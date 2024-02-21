b320735659db5c3012e650e2237ae204782c228e
Mohammed Zaid <mohammedzee98@gmail.com>
2024-02-21 14:46:33 +0530

    loader was added and useLoader hook used

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -8,6 +8,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import GitHub from './components/GitHub/GitHub.jsx'
+import Github, {githubInfoLoader} from './components/GitHub/GitHub.jsx'
 
 
 
@@ -44,6 +45,11 @@ const router = createBrowserRouter(
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
       <Route path='user/:userid' element={<User />} />
+      <Route 
+       loader={githubInfoLoader}
+       path='github'
+       element={<Github />}
+      />
     </Route>
   )
 )
