75dcda183156f79e55cdef76b95e4d6ba1ad71b6
Mohammed Zaid <mohammedzee98@gmail.com>
2024-02-21 20:36:17 +0530

    theme function created and content wrapped in themeProvider

--- 09themeSwitch/src/App.jsx
+++ 09themeSwitch/src/App.jsx
@@ -1,9 +1,27 @@
 import './App.css'
+import { ThemeProvider } from './Contexts/Theme'
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+   // actual change in theme
+
+   useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
 
   return (
-<div className="flex flex-wrap min-h-screen items-center">
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+    <div className="flex flex-wrap min-h-screen items-center">
     <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
           {/*themeBtn */}
@@ -14,7 +32,8 @@ function App() {
            {/* Card*/}
         </div>
     </div>
-</div>
+    </div>
+    </ThemeProvider>
   )
 }
 
