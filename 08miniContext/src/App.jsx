ca99e9c6e1181bbf46ac99e6f7ba875299460484
Mohammed Zaid <mohammedzee98@gmail.com>
2024-02-21 18:56:51 +0530

    user context wrapped and access given

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import UserContextProvider from "./Context/UserContextProvider"
+
 
 function App() {
 
   return (
-    <>
+    <UserContextProvider>
     <h1>React with Zaid</h1>
-    </>
+    </UserContextProvider>
   )
 }
 
