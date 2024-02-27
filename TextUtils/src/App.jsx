2e04fa9aca8a838dda7ea08fca8eb03b98db30bb
Mohammed Zaid <mohammedzee98@gmail.com>
2024-02-27 18:30:20 +0530

    Route closing tag added

--- TextUtils/src/App.jsx
+++ TextUtils/src/App.jsx
@@ -6,7 +6,7 @@ import React, { useState } from 'react';
 import Alert from './components/Alert';
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route
 } from "react-router-dom";
 
@@ -43,16 +43,21 @@ function App() {
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
     <Alert alert={alert}/>
     <div className="container my-3">
-    <Switch>
+    <Routes>
     {/* /users --> Component 1
         /users/home --> Component 2 */}
-          <Route exact path="/about">
-            <About mode={mode} />
-          </Route>
-          <Route exact path="/">
-            <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>
-          </Route>
-    </Switch>
+          <Route exact path="/about" element={<About mode={mode} />} />
+          <Route
+           exact path="/"
+           element={
+             <TextForm 
+             showAlert={showAlert} 
+             heading="Try TextUtils - word counter, character counter, remove extra spaces" 
+             mode={mode}
+               />
+             }
+         />
+    </Routes>
     </div>
     </Router>
     </> 
