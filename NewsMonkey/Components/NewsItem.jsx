40620ddd5b2b07c2811f5242c4c5cee439583c60
Mohammed Zaid <mohammedzee98@gmail.com>
2024-02-28 16:50:27 +0530

    newsitem added

---
+++ NewsMonkey/Components/NewsItem.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        This ia a news item.
+      </div>
+    )
+  }
+}
+
+export default NewsItem
