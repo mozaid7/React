1e2e79b39e02cb1bc8fed9a46176e979c1ee4be6
Mohammed Zaid <mohammedzee98@gmail.com>
2024-02-28 19:05:11 +0530

    added new variables using this.props and this.state

--- NewsMonkey/Components/NewsItem.jsx
+++ NewsMonkey/Components/NewsItem.jsx
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description} = this.props;
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
-      <div>
+      <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-        <img src="https://cdn.24.co.za/files/Cms/General/d/10743/97d776dc91734e98906c0e1b7f3b1afa.jpg" className="card-img-top" alt="..."/>
+        <img src={imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+            <a href="/newsdetail/" className="btn btn-sm btn-primary">Read More</a>
         </div>
         </div>
       </div>
