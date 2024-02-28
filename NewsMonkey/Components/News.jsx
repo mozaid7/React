1abe74394151137c8d6cbcfc081fb41eed29dab6
Mohammed Zaid <mohammedzee98@gmail.com>
2024-02-28 18:10:40 +0530

    added image and editing in layout

--- NewsMonkey/Components/News.jsx
+++ NewsMonkey/Components/News.jsx
@@ -4,9 +4,19 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news component
-        <NewsItem title="myTitle" description="mydesc"/>
+      <div className='container my-3'>
+        <h2>NewsMonkey - Top Headlines</h2>
+        <div className="row">
+            <div className="col md-3">
+                <NewsItem title="myTitle" description="mydesc"/>
+            </div>
+            <div className="col md-3">
+                <NewsItem title="myTitle" description="mydesc"/>
+            </div>
+            <div className="col md-3">
+                <NewsItem title="myTitle" description="mydesc"/>
+            </div>
+        </div>
       </div>
     )
   }
--- NewsMonkey/Components/NewsItem.jsx
+++ NewsMonkey/Components/NewsItem.jsx
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
     return (
       <div>
         <div className="card" style={{width: "18rem"}}>
-        <img src="..." className="card-img-top" alt="..."/>
+        <img src="https://cdn.24.co.za/files/Cms/General/d/10743/97d776dc91734e98906c0e1b7f3b1afa.jpg" className="card-img-top" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
