cafcb2422d1211f9bd3983e208104e63be41e377
Mohammed Zaid <mohammedzee98@gmail.com>
2024-02-28 16:42:12 +0530

    bug in navbar removed

--- NewsMonkey/Components/Navbar.jsx
+++ NewsMonkey/Components/Navbar.jsx
@@ -35,9 +35,8 @@ export class Navbar extends Component {
         </li>
       </ul>
       <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <input  className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button className="btn btn-outline-success" type="submit">Search</button>
-        </input>
       </form>
     </div>
   </div>
