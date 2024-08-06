6de1be14ec9c57bdeb9ea01f6c54c2415f5a2af5
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-07 01:29:03 +0530

    database on MongoDb created for TodoSchema

---
+++ React 0-100/NewYearApp/Backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://mozaid7:mohammedzaid98@cluster7.v0zmtq7.mongodb.net/Todo")
+
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
+})
+
+const todo = mongoose.model('todo_data', todoSchema);
+module.exports = {
+    todo
+}
