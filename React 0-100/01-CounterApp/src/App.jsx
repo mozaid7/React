432f11729209c54595502c8ab220587a0801f645
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-06 12:30:00 +0530

    updation of useState hook

--- React 0-100/01-CounterApp/src/App.jsx
+++ React 0-100/01-CounterApp/src/App.jsx
@@ -4,15 +4,22 @@ import { useState } from 'react'
 function App() {
   const [count, setCount] = useState(0);
 
-  function onClickHandler() {
-    setCount(count + 1);
-  }
-
   return (
     <div>
-      <button onClick={onClickHandler}>Counter {count}</button>
+      <CustomButton count={count} setCount={setCount}></CustomButton>
     </div>
   )
+}
+
+function CustomButton(props) {
+
+  function onClickHandler() {
+    props.setCount(count + 1);
+  }
+
+  return <button onClick={onClickHandler}>
+    Counter {props.count}
+  </button>
 }
 
 export default App
