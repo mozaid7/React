e67ceda9b8e2bef9d55c13ed7056b1dbffe38c0e
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-07 01:17:41 +0530

    packages added

--- React 0-100/NewYearApp/Backend/index.js
+++ React 0-100/NewYearApp/Backend/index.js
@@ -1,9 +1,18 @@
-const express = require("express");
+const express = require('express');
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 
 app.use(express.json());
-app.post("/todo", fuction(req, res) {
 
+app.post("/todo", fuction(req, res) {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong input",
+        })
+        return;
+    }
 })
 
 app.get("/todos", fuction(req, res) {
@@ -11,5 +20,12 @@ app.get("/todos", fuction(req, res) {
 })
 
 app.put("/completed", fuction(req, res) {
-    
+    const updatePayload = req.body;
+    const parsedPayload = updateTodoTodo.safeParse(updatePayload);
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong input",
+        })
+        return;
+    }
 })
