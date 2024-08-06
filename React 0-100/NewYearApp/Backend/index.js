a8cd3a4980f3df920cb53a44d844b50751b33ae3
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-07 00:32:53 +0530

    get,post & put request created

---
+++ React 0-100/NewYearApp/Backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.post("/todo", fuction(req, res) {
+
+})
+
+app.get("/todos", fuction(req, res) {
+    
+})
+
+app.put("/completed", fuction(req, res) {
+    
+})
